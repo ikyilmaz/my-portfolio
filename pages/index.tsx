f7fda94b5b680e7da9decc47a3db409927cf099b
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-10 05:27:56 +0300

    add homepage

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { gsap } from "gsap";
+import type { NextPage } from "next";
+import React, { useState } from "react";
+import { useEffect, useRef } from "react";
+import styled from "styled-components";
+import DefaultLayout from "../layouts/DefaultLayout";
+import HomePage from "./home";
+
+const Home: NextPage = () => {
+  return <HomePage />;
+};
+
+export default Home;
