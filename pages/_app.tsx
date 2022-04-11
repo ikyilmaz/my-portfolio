7b0fc84e9b8bd1424184860e085b58c9b9f74fb8
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-12 00:23:40 +0300

    add some css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,38 @@ import type { AppProps } from "next/app";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import React from "react";
 import { theme } from "../shared/theme";
+import Head from "next/head";
 
 const GlobalStyle = createGlobalStyle`
+
+  * {
+    /* cursor: none; */
+    box-sizing: border-box;
+    color: white;
+  }
+
   html body {
     background-color: ${(props) => props.theme.colors.bgColor};
+    font-family: "JetBrains Mono", sans-serif;
+    margin: 0;
+  }
+
+
+  html {
+    font-size: 10px;
+  }
+
+  body {
+    overflow-x: hidden;
   }
 `;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
