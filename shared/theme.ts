303547e74c66c051f2341e33c61663e529c3f37e
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-12 00:24:12 +0300

    add fontSize and spacing vars

--- shared/theme.ts
+++ shared/theme.ts
@@ -11,4 +11,23 @@ export const theme: DefaultTheme = {
     white: "#fff",
     black: "#0f0f0f",
   },
+
+  fontSize: {
+    desktop: {
+      headingPrimary: "3.6rem",
+      headingSecondary: "2rem",
+      badge: "1.6rem",
+      paragraph: "2rem",
+      button: "1.6rem",
+    },
+
+    mobile: {
+      headingPrimary: "2.4rem",
+      headingSecondary: "1.6rem",
+      badge: "1.2rem",
+      paragraph: "1.6rem",
+      button: "1.2rem",
+    },
+  },
+  spacing: ["0.4rem", "0.8rem", "1.6rem", "3.2rem", "6.4rem"],
 };
