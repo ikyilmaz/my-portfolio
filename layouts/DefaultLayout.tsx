e21918e74d0f6bf92d9e78798e8317134ae82689
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-12 02:11:12 +0300

    remove nav

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -3,24 +3,18 @@ import type { NextPage } from "next";
 import React from "react";
 import styled from "styled-components";
 import { AnimatedCursor } from "../components/AnimatedCursor";
+import { NavigationBar } from "../components/NavigationBar";
 import { Reveal } from "../components/Reveal";
 
 const Container = styled.div`
   height: 100vh;
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 60px 1fr;
+  grid-template-rows: 80px 1fr;
   gap: 0px 0px;
   grid-template-areas:
     "nav-bar"
     "content";
-
-  .nav {
-    background-color: gainsboro;
-    width: 100%;
-
-    grid-area: "nav-bar";
-  }
 
   .content {
     grid-area: "content";
@@ -37,7 +31,7 @@ const DefaultLayout: NextPage<DefaultLayoutProps> = ({ children }) => {
       <AnimatedCursor />
       <Container>
         <Reveal />
-        <nav></nav>
+        <NavigationBar />
         <div className="content">{children}</div>
       </Container>
     </React.Fragment>
