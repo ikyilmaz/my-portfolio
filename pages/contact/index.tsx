65bfbe4dd4c1a624ee6d7e2bef45578c97406a09
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-12 04:06:59 +0300

    create contact page

---
+++ pages/contact/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import React, { useRef } from "react";
+import DefaultLayout from "../../layouts/DefaultLayout";
+
+const ContactPage: NextPage = () => {
+  return (
+    <DefaultLayout>
+      <p>selamlar</p>
+    </DefaultLayout>
+  );
+};
+
+export default ContactPage;
