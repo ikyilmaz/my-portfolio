028edf36052731fc7e4c44ee43c39f00e8abefa4
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2022-04-13 03:27:28 +0300

    add redirect url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [{source: "/home", destination: "/", permanent: true}]
+  }
 }
 
 module.exports = nextConfig
